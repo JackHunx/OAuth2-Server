839857b4782b5bcb025e1e29d47ec7e3d7d65bec
Jack <king.521.love@gmail.com>
2013-08-09 10:41:44 +0800

    'update'

--- protected/modules/oauth2/controllers/OauthController.php
+++ protected/modules/oauth2/controllers/OauthController.php
@@ -2,15 +2,40 @@
 
 class OauthController extends Controller
 {
-	protected function beforeAction($action)
+    //init
+    public function init()
+    {
+        parent::init();
+        Yii::app()->setComponents(array(
+            'errorHandler' => array(
+                'class' => 'CErrorHandler',
+                'errorAction' => $this->getId() . '/default/error',
+                ),
+            'user' => array(
+                'class' => 'CWebUser',
+                'stateKeyPrefix' => 'gii',
+                'loginUrl' => Yii::app()->createUrl($this->getId() . '/default/login'),
+                ),
+            'widgetFactory' => array('class' => 'CWidgetFactory', 'widgets' => array())), false);
+       Yii::app()->user->loginRequired();
+    }
+    //
+    protected function beforeAction($action)
     {
         //must return true;
         return true;
     }
-    //get storage 
+    //get storage
     protected function getStorage()
     {
         return new OAuth(Yii::app()->params['pdo']);
     }
-    //set storage
+    /**
+     * authorize 
+     * @return boolen true or false
+     */
+    public function authorize()
+    {
+
+    }
 }
