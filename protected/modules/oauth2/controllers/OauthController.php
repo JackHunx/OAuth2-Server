39dc3fc0e6abfd4dd84e5ae0052640e6691a2f83
Jack <jack.hunx@gmail.com>
2013-08-10 15:27:06 +0800

    update

--- protected/modules/oauth2/controllers/OauthController.php
+++ protected/modules/oauth2/controllers/OauthController.php
@@ -2,24 +2,30 @@
 
 class OauthController extends Controller
 {
-    public $defaultAction='authorize';
-    private $loginUrl;
+    /**
+     * default action 
+     */
+    public $defaultAction = 'authorize';
+    /**
+     * the defalut login template file path 
+     */
+    public $loginTemplate = '/default/login';
     //init
     public function init()
     {
         //parent::init();
-//        Yii::app()->setComponents(array(
-//            'errorHandler' => array(
-//                'class' => 'CErrorHandler',
-//                'errorAction' => $this->getId() . '/default/error',
-//                ),
-//            'user' => array(
-//                'class' => 'CWebUser',
-//                'stateKeyPrefix' => 'gii',
-//                'loginUrl' => Yii::app()->createUrl($this->getId() . '/login'),
-//                ),
-//            'widgetFactory' => array('class' => 'CWidgetFactory', 'widgets' => array())), false);
-//       Yii::app()->user->loginRequired();
+        //        Yii::app()->setComponents(array(
+        //            'errorHandler' => array(
+        //                'class' => 'CErrorHandler',
+        //                'errorAction' => $this->getId() . '/default/error',
+        //                ),
+        //            'user' => array(
+        //                'class' => 'CWebUser',
+        //                'stateKeyPrefix' => 'gii',
+        //                'loginUrl' => Yii::app()->createUrl($this->getId() . '/login'),
+        //                ),
+        //            'widgetFactory' => array('class' => 'CWidgetFactory', 'widgets' => array())), false);
+        //       Yii::app()->user->loginRequired();
     }
     //
     protected function beforeAction($action)
@@ -39,35 +45,41 @@ class OauthController extends Controller
     public function actionAuthorize()
     {
         $authorize = $this->login();
-        $this->getStorage()->authorize($authorize,$this->getUserId());
+        $this->getStorage()->authorize($authorize, $this->getUserId());
     }
     /**
-     * set loginUrl
-     * 
-     * @param string $loginUrl where the login template 
+     * @return boolen true or false
      */
-     protected function setLoginUrl($loginUrl=null)
-     {
-        //$this->loginUrl = $loginUrl == null ? Yii::app()
-     }
-     /**
-      * @return boolen true or false
-      */
-     protected function login()
-     {
-        if()
-        $this->renderPartial('/default/login');
+    protected function login()
+    {
+        //if login or not
+        if (Yii::app()->user->isGuest) {
+            if(isset($_POST['LoginForm']))
+            {
+                
+            }
+            $this->renderPartial($this->loginTemplate);
+            Yii::app()->end();
+        } else {
+            //not a guest , login with this logined user or login with a nother user
+            return true;
+        }
+
+
         //return true;
-     }
-     /**
-      * @return mixed null or $user_id
-      */
-     protected function getUserId()
-     {
+    }
+    /**
+     * @return mixed null or $user_id
+     */
+    protected function getUserId()
+    {
         return Yii::app()->user->id == null ? null : Yii::app()->user->id;
-     }
-     
-     
-     
-     
+    }
+    /**
+     * get user info 
+     */
+    protected function getUserInfo()
+    {
+        
+    }
 }
---
+++ protected/modules/oauth2/views/default/login.php
@@ -0,0 +1,5 @@
+<font color="red">This is a login page you must authorize from this page</font>
+<form action="#" method="post">
+<input type="submit" name="authorize" value="yes"></input>
+<input type="submit" name="authorize" value="no"></input>
+</form>
